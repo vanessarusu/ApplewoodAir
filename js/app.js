08df7e2dcd0e1ec2e38689acec2e843932c478de
Vanessa Rusu <vanessarusu@gmail.com>
2016-09-04 01:32:07 -0700

    Slider Fixes
    Replaced Sliders to the basic foundation slider

--- js/app.js
+++ js/app.js
@@ -1,26 +1,12 @@
+$(document).ready(function(){
 $(document).foundation();
+// $(window).on('resize', Foundation.lib_methods.throttle(function () {
+//        $(document).foundation();
+// }, 300));
+});
 
+// console.log('hahaha');
 
 if(typeof window.getComputedStyle(document.body).backgroundBlendMode == 'undefined') {
     document.documentElement.className += "no-background-blend-mode";
 }
-
-jQuery( document ).ready(function( $ ) {
-        $( '#my-slider' ).sliderPro({
-        	width: '100%',
-        	height: 597,
-        	buttons: false,
-        	thumbnailsContainerWidth: '100%',
-        	thumbnailHeight: 50,
-        	autoplay: true,
-        	touchSwipe: true
-        });
-        $( '#testimonialSlider' ).sliderPro({
-        	width: '100%',
-        	height: 300,
-        	buttons: false,
-        	autoplay: false,
-        	touchSwipe: true,
-        	arrows: true
-        });
-    });
